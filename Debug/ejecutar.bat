f46ad5c478b2ce0ac2d7900fcdf193bfc0249c71
gvillamayor <gvillamayor@gmail.com>
2014-06-26 11:08:27 -0300

    Inicial sin BIGGEST_OBJECT

---
+++ Debug/ejecutar.bat
@@ -0,0 +1,8 @@
+rem CarDetection Autos/Auto20.jpg checkcas.xml cas1.xml cas2.xml cas3.xml cas4.xml
+rem CarDetection Autos/Auto21.jpg checkcas.xml cas1.xml cas2.xml cas3.xml cas4.xml
+rem CarDetectVideo 20140609154811458.wmv checkcas.xml cas1.xml cas2.xml cas3.xml cas4.xml
+CarDetectVideo 20140609154448904.wmv checkcas.xml cas1.xml cas2.xml cas3.xml cas4.xml
+CarDetectVideo 20140609154702476.wmv checkcas.xml cas1.xml cas2.xml cas3.xml cas4.xml
+CarDetectVideo 20140609154811458.wmv checkcas.xml cas1.xml cas2.xml cas3.xml cas4.xml
+CarDetectVideo 20140609154815264.wmv checkcas.xml cas1.xml cas2.xml cas3.xml cas4.xml
+pause
